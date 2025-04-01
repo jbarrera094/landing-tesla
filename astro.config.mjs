2777db875b3d8195cf6cc1a2418d5257e7f2d33b
jbarrera094 <jbarrera094@gmail.com>
2025-03-31 22:43:14 -0500

    Actualizar la configuración del sitio en Astro para reflejar la nueva estructura de despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jbarrera094.github.io/landing-tesla/",
+  site: "https://jbarrera094.github.io",
+  base: "landing-tesla",
   vite: {
     plugins: [tailwindcss()],
   },
